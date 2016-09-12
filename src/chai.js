8b2b4bfa1224cf16a0411336776f0e95587665b9
Michael J. Bennett <michael.bennett@rangle.io>
2016-09-12 10:31:01 -0400

    Basic node v6 starter

---
+++ src/chai.js
@@ -0,0 +1,46 @@
+/**
+ * These functions are used as helpers for unit tests
+ * @module chai
+ */
+
+/* eslint prefer-const: 0 */ // testing
+let chai = require('chai');
+
+chai.config.includeStack = true;
+
+global.expect = chai.expect;
+
+module.exports = {
+  check,
+  getFail,
+};
+
+/* global describe, it, expect, beforeEach, afterEach */
+/**
+  @param {function()} done
+  @param {function()} fn
+*/
+function check(done, fn) {
+  try {
+    fn();
+    done();
+  } catch (err) {
+    done(err);
+  }
+}
+
+/**
+  @param {function()} done
+  @return {function()}
+*/
+function getFail(done) {
+  function fail(err) {
+    if (err instanceof Error) {
+      done(err);
+    } else {
+      done(new Error('this case should not happen'));
+    }
+  }
+  return fail;
+}
+
